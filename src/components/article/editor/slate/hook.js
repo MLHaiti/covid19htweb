b4218e0d01fd2938f495e85cc2ddf1a1bbbc9b2a
Emmanuel Fleurine <emmanuelfleurine@gmail.com>
2020-04-30 06:14:56 -0400

    Reset toolbar on exit

--- src/components/article/editor/slate/hook.js
+++ src/components/article/editor/slate/hook.js
@@ -1,26 +1,8 @@
 import useSWR from "swr";
 
-const initialStore = {
-  toolbar: {
-    height: 57,
-    top: 10,
-  },
-  afterEditor: {
-    height: 1,
-    top: 20,
-  },
-};
-
-export const useScroll = () => {
-  const { data } = useSWR("articleEditorSrolling", {
-    initialData: initialStore,
-  });
-  return data;
-};
-
 export const useToolbarPosition = () => {
   const { data } = useSWR("articleEditorToolbarPosition", {
-    initialData: { height: 57, top: 10 },
+    initialData: { height: 0, top: 0 },
   });
   return data;
 };
