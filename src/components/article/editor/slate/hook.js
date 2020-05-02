fff0cc6bfdd9afc7e6502620ea348ffa18a07121
Emmanuel Fleurine <emmanuelfleurine@gmail.com>
2020-05-01 23:38:53 -0400

    Generic scrolling notification system

--- src/components/article/editor/slate/hook.js
+++ src/components/article/editor/slate/hook.js
@@ -1,12 +1,5 @@
 import useSWR from "swr";
 
-export const useToolbarPosition = () => {
-  const { data } = useSWR("articleEditorToolbarPosition", {
-    initialData: { height: 0, top: 0 },
-  });
-  return data;
-};
-
 export const useScrollTop = () => {
   const { data } = useSWR("articleEditorScrollTop", {
     initialData: { scrollTop: 0 },
