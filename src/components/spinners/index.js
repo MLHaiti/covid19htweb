3a384c5539d5b392a2f44bc6579553a0177c9686
Emmanuel Fleurine <emmanuelfleurine@gmail.com>
2020-05-27 14:25:42 -0400

    New spinners
    Taken from
    
    https://github.com/tienpham94/react-awesome-spinners
    
    and adapter to emotion

---
+++ src/components/spinners/index.js
@@ -0,0 +1,7 @@
+// Ref => https://github.com/tienpham94/react-awesome-spinners
+export * from "./DualRing";
+export * from "./CircleDot";
+export * from "./HourGlass";
+export * from "./Ripple";
+export * from "./Spinner";
+export * from "./Roller";
